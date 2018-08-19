b2dfee2d3878d347d027e55a2cc8e22f84331e20
gudwlsg1 <33974894+gudwlsg1@users.noreply.github.com>
2018-08-19 11:57:41 +0900

    커밋
    커밋

--- NRoom/NRoom/NRoom/App.xaml.cs
+++ NRoom/NRoom/NRoom/App.xaml.cs
@@ -2,29 +2,29 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-[assembly: XamlCompilation (XamlCompilationOptions.Compile)]
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace NRoom
 {
     public partial class App : Application
     {
-        public App ()
+        public App()
         {
             InitializeComponent();
 
             MainPage = new MainPage();
         }
 
-        protected override void OnStart ()
+        protected override void OnStart()
         {
             // Handle when your app starts
         }
 
-        protected override void OnSleep ()
+        protected override void OnSleep()
         {
             // Handle when your app sleeps
         }
 
-        protected override void OnResume ()
+        protected override void OnResume()
         {
             // Handle when your app resumes
         }
