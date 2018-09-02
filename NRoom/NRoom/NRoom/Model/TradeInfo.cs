fc12cc8edc78bc40c0e9d1e41bed11d6bcdf6d79
MEIAMLAMBS <sunion10@naver.com>
2018-09-02 10:48:40 +0900

    Network 일부분 수정

--- NRoom/NRoom/NRoom/Model/TradeInfo.cs
+++ NRoom/NRoom/NRoom/Model/TradeInfo.cs
@@ -6,12 +6,36 @@ namespace NRoom.Model
 {
     public class TradeInfo
     {
-        public string Price { get; set; }
+        /*public string Price { get; set; }
         public string CompletionYear { get; set; }
         public string RegistrationDate { get; set; }
         public string Address { get; set; }
         public string Name { get; set; }
         public string Area { get; set; }
-        public string LocalCode { get; set; }
+        public string LocalCode { get; set; }*/
+        public string 거래금액 { get; set; }
+        public string 건축년도 { get; set; }
+        public string 년 { get; set; }
+        public string 도로명 { get; set; }
+        public string 도로명건물본번호코드 { get; set; }
+        public string 도로명건물부번호코드 { get; set; }
+        public string 도로명시군구코드 { get; set; }
+        public string 도로명일련번호코드 { get; set; }
+        public string 도로명지상지하코드 { get; set; }
+        public string 도로명코드 { get; set; }
+        public string 법정동 { get; set; }
+        public string 법정동본번코드 { get; set; }
+        public string 법정동부번코드 { get; set; }
+        public string 법정동시군구코드 { get; set; }
+        public string 법정동읍면동코드 { get; set; }
+        public string 법정동지번코드 { get; set; }
+        public string 아파트 { get; set; }
+        public string 월 { get; set; }
+        public string 일 { get; set; }
+        public string 일련번호 { get; set; }
+        public string 전용면적 { get; set; }
+        public string 지번 { get; set; }
+        public string 지역코드 { get; set; }
+        public string 층 { get; set; }
     }
 }
---
+++ NRoom/NRoom/NRoom/Network/TradeInfoManager.cs
@@ -0,0 +1,18 @@
+﻿using NRoom.Network.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NRoom.Network
+{
+    public partial class NetworkManager
+    {
+        private const string TRADE_URL = "/home/DetailApartEstate";
+
+        public async Task<NResponse<TradeInfoData>> getTradeInfo()
+        {
+            return null;
+        }
+    }
+}
