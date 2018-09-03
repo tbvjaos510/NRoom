38ced530e59c5e7429e44b72728a5e4c31319d22
gudwlsg1 <33974894+gudwlsg1@users.noreply.github.com>
2018-09-03 15:16:17 +0900

    자세히보기 윤곽잡기

--- NRoom/NRoom/NRoom/View/HomeInfoView.xaml.cs
+++ NRoom/NRoom/NRoom/View/HomeInfoView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,7 +25,7 @@ namespace NRoom.View
 
         private void Positive_Tapped(object sender, EventArgs e)
         {
-
+            ViewDetailInfo.IsVisible = true;
         }
     }
 }
