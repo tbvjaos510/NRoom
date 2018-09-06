4af9069875444ee85c09a08979ac5a2bdfc7cbcc
gudwlsg1 <33974894+gudwlsg1@users.noreply.github.com>
2018-09-06 23:44:07 +0900

    MainUi + 버튼클릭시 이동

--- NRoomAndroid/app/src/main/java/com/example/nroom/nroom/MainActivity.java
+++ NRoomAndroid/app/src/main/java/com/example/nroom/nroom/MainActivity.java
@@ -1,13 +1,27 @@
  package com.example.nroom.nroom;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnMap = (Button) findViewById(R.id.btnMap);
+        btnMap.setOnClickListener((v) -> {
+            Intent intent = new Intent(MainActivity.this, MapActivity.class);
+            startActivity(intent);
+        });
+
+        Button btnList = (Button) findViewById(R.id.btnList);
+        btnList.setOnClickListener((v)->{
+            Intent intent = new Intent(MainActivity.this, ListActivity.class);
+            startActivity(intent);
+        });
     }
 }
