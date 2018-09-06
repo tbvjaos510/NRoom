24859414876bd7c17255c136cd9072533fba65e5
gudwlsg1 <33974894+gudwlsg1@users.noreply.github.com>
2018-09-06 16:38:31 +0900

    Network 연결하는 클래스 생성 - 미완성

---
+++ NRoomAndroid/app/src/main/java/com/example/nroom/nroom/Network/NetworkManager.java
@@ -0,0 +1,46 @@
+package com.example.nroom.nroom.Network;
+
+import android.os.AsyncTask;
+import android.util.JsonReader;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+// https://code.tutsplus.com/ko/tutorials/android-from-scratch-using-rest-apis--cms-27117 네트워크 연결하는 방법 홈페이지
+public class NetworkManager {
+    public static class GetResponseTask extends AsyncTask<URL, Integer, String> {
+        @Override
+        protected String doInBackground(URL... uris) {
+            try {
+                HttpsURLConnection myConnection =
+                        (HttpsURLConnection) uris[0].openConnection();
+                if (myConnection.getResponseCode() == 200) {
+                    InputStreamReader responseBodyReader =
+                            new InputStreamReader(myConnection.getInputStream(), "UTF-8");
+                    JsonReader jsonReader = new JsonReader(responseBodyReader);
+                } else {
+
+                }
+
+            } catch (IOException ignore) {}
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+    }
+    /*public static void GetResponse(final URI uri){
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                HttpsURLConnection myConnection =
+                        (HttpsURLConnection) uri.openConnection();
+            }
+        });
+    }*/
+}
