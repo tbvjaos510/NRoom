b54907f6b9a7828f1a3cdddbce66f30dac2858ee
gudwlsg1 <33974894+gudwlsg1@users.noreply.github.com>
2018-09-08 16:47:03 +0900

    버튼 클릭시 이벤트

--- NRoomAndroid/app/src/main/java/com/nroom/activity/ListActivity.java
+++ NRoomAndroid/app/src/main/java/com/nroom/activity/ListActivity.java
@@ -1,7 +1,10 @@
 package com.nroom.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.nroom.R;
 
@@ -11,5 +14,11 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        Button btnLocal = findViewById(R.id.btnLocal);
+        btnLocal.setOnClickListener(view -> {
+            /*Intent intent = new Intent(this, MapActivity.class);
+            startActivity(intent);*/
+        });
     }
 }
