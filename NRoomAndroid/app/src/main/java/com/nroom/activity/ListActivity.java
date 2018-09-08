a0b50a4b3a0770e0dfeb3e9464f308e3f6d1ed3c
gudwlsg1 <33974894+gudwlsg1@users.noreply.github.com>
2018-09-08 20:22:13 +0900

    gps 받아오기 되겠지?

--- NRoomAndroid/app/src/main/java/com/nroom/activity/ListActivity.java
+++ NRoomAndroid/app/src/main/java/com/nroom/activity/ListActivity.java
@@ -1,14 +1,45 @@
 package com.nroom.activity;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.nroom.R;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ListActivity extends AppCompatActivity {
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            double longitude = location.getLongitude();
+            double latitude = location.getLatitude();
 
+            String provider = location.getProvider();
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +50,29 @@ public class ListActivity extends AppCompatActivity {
             /*Intent intent = new Intent(this, MapActivity.class);
             startActivity(intent);*/
         });
+
+        ToggleButton btnGps = findViewById(R.id.btnGps);
+        final LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        btnGps.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                try{
+                    if(btnGps.isChecked()){
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 1,mLocationListener);
+                        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, // 등록할 위치제공자
+                                100, // 통지사이의 최소 시간간격 (miliSecond)
+                                1, // 통지사이의 최소 변경거리 (m)
+                                mLocationListener);
+                    }else{
+                        locationManager.removeUpdates(mLocationListener);
+                    }
+                }catch (SecurityException ex){
+
+                }
+            }
+        });
+
     }
+
 }
